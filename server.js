e76375e4f1e70b0553b845e3ecc07ebdbaa15066
Jared <jaredgreen10@gmail.com>
2020-06-22 16:33:16 -0400

    added basic routing

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ var port = process.env.PORT || 8080;
 
 var app = express();
 
-/*
-app.get('/', function {req, res}
-    
+app.get('/', function (req, res) {
+    res.render('signIn');
 });
-*/
+
+app.get('/signUp', function(req, res) {
+      res.render('signUp');
+});
+
+app.get('/homepage', function(req, res) {
+    res.render('homepage');
+});
+
+app.get('/sendData', function(req, res) {
+    res.render('sendData');
+});
+
+app.get('/viewData', function(req, res) {
+    res.render('viewData');
+});
 
 app.listen(8080);
 console.log("app listening on port 8080");
