ae06b47a387d2a95854fe4f64b1ac3a6fdff5638
Jared <jaredgreen10@gmail.com>
2020-06-22 16:23:54 -0400

    stubbed files

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require{'express'};
+var port = process.env.PORT || 8080;
+
+var app = express();
+
+/*
+app.get('/', function {req, res}
+    
+});
+*/
+
+app.listen(8080);
+console.log("app listening on port 8080");
