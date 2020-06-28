5233ddf3e8ef50dc4b7e2355ea31756d37b5ca4e
Jared <jaredgreen10@gmail.com>
2020-06-28 15:02:33 -0400

    modified controller erquire statement

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const signInController = require('./controllers/signInController');
-const signUpController = require('./controllers/signUpController');
+const signUpController = require('./controllers/signUpController.js');
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
