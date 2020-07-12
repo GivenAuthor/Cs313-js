a6c81689d83e29fd0571497f60be7fd4edcdda2f
Jared <jaredgreen10@gmail.com>
2020-07-11 20:34:59 -0400

    passing callback

--- models/homepageModel.js
+++ models/homepageModel.js
@@ -6,13 +6,13 @@ const pool = new Pool({
   ssl: true
 });
 
-function dbRequest() {
+function dbRequest(req, res, callback) {
   pool.connect();
     console.log('requesting info from db');
     let sql = `SELECT note.note_contents, rating.day_rating, day.day_date
     FROM note
     INNER JOIN day ON note.note_id = day.day_id
-    LEFT JOIN rating ON note.note_id = rating.rating_id`;
+    LEFT JOIN rating ON note.note_id = rating.rating_id;`;
     pool.query(sql, (err, res) => {
       if (err) {
         console.log('dbRequestError:');
